14584a1fb8f53a8cc3dc6a2e55463118cf8d1200
Comparo <mati.agobian@gmail.com>
2023-06-22 23:50:23 -0300

    desafio

--- src/daos/mongo/user.dao.js
+++ src/daos/mongo/user.dao.js
@@ -1,15 +1,17 @@
-import { userModel } from "../models/user.models.js";
+import { createHash, isValidPassword } from "../../../../clases-main/src/path.js";
+import { userModel } from "../models/user.models.js"; 
+
 
 export default class UserDao {
   async createUser(user) {
     try {
-      const { email, password } = user;
+      const { first_name, last_name,  age,  email, password } = user;
       const existUser = await userModel.find({email});
       if(existUser.length === 0){
         if(email === 'adminCoder@coder.com' && password === 'adminCoder123'){
-          return await userModel.create({...user, role: 'admin'});
+          return await userModel.create({...user, password: createHash(password), role: 'admin'});
         } else {
-          const newUser = await userModel.create(user);
+          const newUser = await userModel.create({...user, password: createHash(password)});
           return newUser
         }
       } else {
@@ -21,18 +23,45 @@ export default class UserDao {
     }
   }
 
-  async loginUser(user){
+  async loginUser(user) {
     try {
       const { email, password } = user;
-      const userExist = await userModel.find({email, password});
-      if(userExist.length !== 0){
-        return userExist
-      } else {
-        return null
-      }
+      const userExist = await userModel.findOne({ email })
+     
+      if (userExist) {
+        const isValid = isValidPassword(password, userExist);
+        if (!isValid)return false
+        else return userExist
+      } return false
+    } catch (error) {
+      console.log(error);
+      throw new Error(error);
+    }
+  }
+
+  async getById(id){
+    try {
+      const userExist = await userModel.findById(id)
+      // console.log(userExist);
+      if(userExist){
+       return userExist
+      } return false
+    } catch (error) {
+      console.log(error)
+      // throw new Error(error)
+    }
+  }
+
+  async getByEmail(email){
+    try {
+      const userExist = await userModel.findOne({email}); 
+      console.log('user::::', userExist)
+      if(userExist){
+       return userExist
+      } return false
     } catch (error) {
       console.log(error)
       throw new Error(error)
     }
   }
-}
+}  
