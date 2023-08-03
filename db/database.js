657f5c460537fba81d9f29039573c76a81ebab60
Comparo <mati.agobian@gmail.com>
2023-08-03 19:32:03 -0300

    Eliminacion de datos

--- db/database.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose'
-
-const conectionString = 'mongodb+srv://octavio:octavio@clusterecommerce.xka9yxf.mongodb.net/Ecommerce?retryWrites=true&w=majority'
-try {
-    await mongoose.connect(conectionString)
-    console.log('Conectado a Mongoose')
-} catch (error) {
-    console.log(error)
-}
